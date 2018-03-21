956bf1baa5667e3d912b02ee6ceb5e8d8709e9c2
Peter Broadhurst <peterbroadhurst@peters-mbp-2.home>
2018-03-21 22:41:07 +0000

    Constellation node needs to wait for tm.conf to be generated

--- constellation/start.sh
+++ constellation/start.sh
@@ -10,5 +10,15 @@ set -e
 ### Configuration Options
 TMCONF=/qdata/constellation/tm.conf
 
+#
+# we cannot start until tm.conf is available
+#
+echo "[*] TMCONF=$TMCONF"
+while [ ! -f $TMCONF ]
+do
+  sleep 2
+done
+
 echo "[*] Starting Constellation node"
 nohup /usr/local/bin/constellation-node $TMCONF -v3 2>>/qdata/logs/constellation.log
+
